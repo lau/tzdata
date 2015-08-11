4a30f8e5f7fd568db972dedfb4361338953fff00
Lau Taarnskov <lau@taarnskov.com>
2015-08-11 13:59:05 -0300

    Change version for ETS/auto. Skipping 0.2 through 0.4 to make way for breaking changes while keeping old structure.

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Tzdata.Mixfile do
   def project do
     [app: :tzdata,
      name: "tzdata",
-     version: "0.1.6",
+     version: "0.5.0-dev",
      elixir: "~> 1.0 or ~> 1.1",
      package: package,
      description: description,
