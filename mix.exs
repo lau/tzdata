da689341113b63885a1f9db7993b8de82b075572
Lau Taarnskov <lau@taarnskov.com>
2015-10-07 10:04:09 -0500

    In mix.exs rename contributors to maintainers to satisfy hex

--- mix.exs
+++ mix.exs
@@ -30,7 +30,7 @@ defmodule Tzdata.Mixfile do
 
   defp package do
     %{ licenses: ["MIT"],
-       contributors: ["Lau Taarnskov"],
+       maintainers: ["Lau Taarnskov"],
        links: %{ "GitHub" => "https://github.com/lau/tzdata"},
        files: ~w(lib priv mix.exs README* LICENSE*
                  license* CHANGELOG* changelog* src source_data) }
