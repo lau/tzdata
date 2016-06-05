7d40878916d51781a0e2ea818c048a986cd126e9
Lau Taarnskov <lau@taarnskov.com>
2016-06-05 19:31:43 -0300

    Set logger config to use UTC

--- config/config.exs
+++ config/config.exs
@@ -2,5 +2,6 @@
 # and its dependencies with the aid of the Mix.Config module.
 use Mix.Config
 
+config :logger, utc_log: true
 config :tzdata, :autoupdate, :enabled
 #config :tzdata, :data_dir, "/etc/elixir_tzdata_storage"
