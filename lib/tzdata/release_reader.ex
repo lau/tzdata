40df499cf0e70559cb1977452b4e08d42344038d
Lau Taarnskov <lau@taarnskov.com>
2018-06-21 23:59:00 +0200

    Move storage of periods in ETS from set to bag. WIP The reason is to improve performance. So far the reading functions have not changed to improve performance yet.

--- lib/tzdata/release_reader.ex
+++ lib/tzdata/release_reader.ex
@@ -36,12 +36,27 @@ defmodule Tzdata.ReleaseReader do
 
   defp do_periods_for_zone(zone) do
     case lookup_periods_for_zone(zone) do
-      [{_, period} | _] -> period
+      periods when is_list(periods) -> periods
+        |> Enum.sort_by(fn period -> elem(period, 1) |> delimiter_to_number() end)
       _ -> nil
     end
   end
   defp lookup_periods_for_zone(zone) when is_binary(zone), do: simple_lookup(String.to_atom zone)
   defp lookup_periods_for_zone(_), do: []
+
+  @doc !"""
+  Hack which is useful for sorting periods. Delimiters can be integers representing
+  gregorian seconds or :min or :max. By converting :min and :max to integers, they are
+  easier to sort. It is assumed that the fake numbers they are converted to are far beyond
+  numbers used.
+  TODO: Instead of doing this, do the sorting before inserting. When reading from a bag the order
+  should be preserved.
+  """
+  @very_high_number_representing_gregorian_seconds 9315537984000
+  @low_number_representing_before_year_0 -1
+  defp delimiter_to_number(:min), do: @low_number_representing_before_year_0
+  defp delimiter_to_number(:max), do: @very_high_number_representing_gregorian_seconds
+  defp delimiter_to_number(integer) when is_integer(integer), do: integer
 
   defp current_release_from_table do
     :ets.lookup(:tzdata_current_release, :release_version) |> hd |> elem(1)
