c97204e6f20384839f4cc53d22b6abe68421702b
Ley Missailidis <moisis.langley@missailidis.ca>
2018-12-31 04:16:18 -0800

    Ran mix format on httpclient file

--- lib/tzdata/http_client.ex
+++ lib/tzdata/http_client.ex
@@ -1,20 +1,18 @@
 defmodule Tzdata.HttpClient do
   require Logger
 
-
   @spec get(binary() | charlist()) :: {:ok, integer(), any(), any()}
   def get(url) when is_binary(url) do
     String.to_charlist(url) |> get()
   end
 
   def get(url) when is_list(url) do
-   request = {url, []}
+    request = {url, []}
 
-   {:ok, {{ _, response, _}, headers, body}} = :httpc.request(:get, request, http_options(), [])
+    {:ok, {{_, response, _}, headers, body}} = :httpc.request(:get, request, http_options(), [])
 
-   {:ok, response, headers, :erlang.list_to_binary(body)}
+    {:ok, response, headers, :erlang.list_to_binary(body)}
   end
-
 
   @spec head(binary() | charlist()) :: {:ok, integer(), any()}
   def head(url) when is_binary(url) do
@@ -22,12 +20,11 @@ defmodule Tzdata.HttpClient do
   end
 
   def head(url) when is_list(url) do
-   request = {url, []}
+    request = {url, []}
 
-   {:ok, {{ _ ,response, _ }, headers,[] }} = :httpc.request(:head, request, http_options(), [])
+    {:ok, {{_, response, _}, headers, []}} = :httpc.request(:head, request, http_options(), [])
 
-   {:ok, response, headers}
-
+    {:ok, response, headers}
   end
 
   defp http_options() do
@@ -35,9 +32,8 @@ defmodule Tzdata.HttpClient do
   end
 
   defp ssl_options() do
-     local_storage = Tzdata.Util.data_dir() |> String.to_charlist()
+    local_storage = Tzdata.Util.data_dir() |> String.to_charlist()
 
-     [{:verify, :verify_peer}, {:cacertfile, local_storage ++ '/cacert/cacert.pem'}]
+    [{:verify, :verify_peer}, {:cacertfile, local_storage ++ '/cacert/cacert.pem'}]
   end
-
 end
