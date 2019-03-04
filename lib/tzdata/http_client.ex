5e4b41693387b5fc8ef969ff6d5dbe5a96ec6e85
Wojtek Mach <wojtek@wojtekmach.pl>
2019-03-04 22:55:14 +0100

    Update lib/tzdata/http_client.ex

--- lib/tzdata/http_client.ex
+++ lib/tzdata/http_client.ex
@@ -1,19 +1,21 @@
 defmodule Tzdata.HTTPClient do
   @moduledoc """
   Behaviour for HTTP client used by Tzdata.
+
+  See "HTTP Client" section in README.md for more information.
   """
 
   @type status() :: non_neg_integer()
 
   @type headers() :: [{header_name :: String.t(), header_value :: String.t()}]
 
-  @type body :: binary()
+  @type body() :: binary()
 
-  @type option :: {:follow_redirect, boolean}
+  @type option() :: {:follow_redirect, boolean}
 
-  @callback get(url :: String.t(), headers, [option]) ::
-              {:ok, {status, headers, body}} | {:error, term()}
+  @callback get(url :: String.t(), headers(), options :: [option]) ::
+              {:ok, {status(), headers(), body()}} | {:error, term()}
 
-  @callback head(url :: String.t(), headers, [option]) ::
-              {:ok, {status, headers}} | {:error, term()}
+  @callback head(url :: String.t(), headers(), options :: [option]) ::
+              {:ok, {status(), headers()}} | {:error, term()}
 end
