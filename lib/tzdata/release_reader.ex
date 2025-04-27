6eebb6e66b1ddd1b50d1f723b375b66400c69dfe
Médi-Rémi Hashim <mediremi@gmail.com>
2025-04-27 16:25:02 +0100

    refactor: use :ets.lookup_element instead of :ets.lookup + elem(1)

--- lib/tzdata/release_reader.ex
+++ lib/tzdata/release_reader.ex
@@ -1,21 +1,32 @@
 defmodule Tzdata.ReleaseReader do
   @moduledoc false
 
-  def rules,                  do: simple_lookup(:rules) |> hd |> elem(1)
-  def zones,                  do: simple_lookup(:zones) |> hd |> elem(1)
-  def links,                  do: simple_lookup(:links) |> hd |> elem(1)
-  def zone_list,              do: simple_lookup(:zone_list) |> hd |> elem(1)
-  def link_list,              do: simple_lookup(:link_list) |> hd |> elem(1)
-  def zone_and_link_list,     do: simple_lookup(:zone_and_link_list) |> hd |> elem(1)
-  def archive_content_length, do: simple_lookup(:archive_content_length) |> hd |> elem(1)
-  def release_version,        do: simple_lookup(:release_version) |> hd |> elem(1)
-  def leap_sec_data,          do: simple_lookup(:leap_sec_data) |> hd |> elem(1)
-  def by_group,               do: simple_lookup(:by_group) |> hd |> elem(1)
-  def modified_at,            do: simple_lookup(:modified_at) |> hd |> elem(1)
+  def rules, do: simple_lookup_element(:rules) |> List.first()
+  def zones, do: simple_lookup_element(:zones) |> List.first()
+  def links, do: simple_lookup_element(:links) |> List.first()
+  def zone_list, do: simple_lookup_element(:zone_list) |> List.first()
+  def link_list, do: simple_lookup_element(:link_list) |> List.first()
+  def zone_and_link_list, do: simple_lookup_element(:zone_and_link_list) |> List.first()
+  def archive_content_length, do: simple_lookup_element(:archive_content_length) |> List.first()
+  def release_version, do: simple_lookup_element(:release_version) |> List.first()
+  def leap_sec_data, do: simple_lookup_element(:leap_sec_data) |> List.first()
+  def by_group, do: simple_lookup_element(:by_group) |> List.first()
+  def modified_at, do: simple_lookup_element(:modified_at) |> List.first()
 
-  defp simple_lookup(key) do
-    :ets.lookup(current_release_from_table() |> table_name_for_release_name, key)
+  def simple_lookup_element(key) do
+    :ets.lookup_element(
+      table_name_for_current_release(),
+      key,
+      2,
+      []
+    )
   end
+
+  def simple_lookup(key),
+    do: :ets.lookup(table_name_for_current_release(), key)
+
+  defp table_name_for_current_release(),
+    do: current_release_from_table() |> table_name_for_release_name
 
   def zone(zone_name) do
     {:ok, zones()[zone_name]}
@@ -37,18 +48,13 @@ defmodule Tzdata.ReleaseReader do
   end
 
   def has_modified_at? do
-    simple_lookup(:modified_at) != []
+    simple_lookup(:modified_at) |> Enum.any?()
   end
 
   defp do_periods_for_zone(zone) do
-    case lookup_periods_for_zone(zone) do
-      periods when is_list(periods) ->
-        periods
-        |> Enum.sort_by(fn period -> elem(period, 1) |> delimiter_to_number() end)
-
-      _ ->
-        nil
-    end
+    zone
+    |> lookup_periods_for_zone()
+    |> Enum.sort_by(fn period -> elem(period, 1) |> delimiter_to_number() end)
   end
 
   defp lookup_periods_for_zone(zone) when is_binary(zone),
