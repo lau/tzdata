c1e4072787ac7073c5a9eeb93936c53bbbd1efe3
Matt Wynne <matt.wynne@mechanical-orchard.com>
2026-02-10 16:35:28 -0800

    Correct pattern matching in do_latest_file_size_by_head
    Fix where pattern matching expected a flat
    3-tuple {tag, status, headers} but HTTPClient behaviour
    returns nested structure {:ok, {status, headers}}.
    
    This causes a FunctionClauseError when calling
    latest_file_size_by_head, silently falling back to GET requests.
    
    Adds sham to allow for testing
    
    Co-authored-by: Ryan Spore <ryan@mechanical-orchard.com>

--- mix.exs
+++ mix.exs
@@ -28,7 +28,9 @@ defmodule Tzdata.Mixfile do
   defp deps do
     [
       {:hackney, "~> 1.17 or ~> 4.0", optional: true},
-      {:ex_doc, "~> 0.21", only: :dev, runtime: false}
+      {:ex_doc, "~> 0.21", only: :dev, runtime: false},
+      {:sham, "~> 1.2", only: :test},
+      {:bandit, "~> 1.0", only: :test}
     ]
   end
 
