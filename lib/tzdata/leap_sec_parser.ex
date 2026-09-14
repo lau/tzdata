b5fa73a481a5d112cc24c41b79edc2574309e72d
Thijs Klaver <thijs.klaver@voiceworks.com>
2026-09-14 06:12:20 +0200

    Move regexes out of module attributes to fix compatibility with OTP 28 (#159)

--- lib/tzdata/leap_sec_parser.ex
+++ lib/tzdata/leap_sec_parser.ex
@@ -26,10 +26,10 @@ defmodule Tzdata.LeapSecParser do
     |> organize_into_map
   end
 
-  @expiry_line_regex ~r/\#\@[\s]+(?<expiry_timestamp>[\d]+)/
   defp uncomment_expiry_line(line) do
-    map = Regex.named_captures(@expiry_line_regex, line)
-
+    expiry_line_regex = ~r/\#\@[\s]+(?<expiry_timestamp>[\d]+)/
+    map = Regex.named_captures(expiry_line_regex, line)
+    
     if map do
       "#{map["expiry_timestamp"]}"
     else
@@ -37,9 +37,9 @@ defmodule Tzdata.LeapSecParser do
     end
   end
 
-  @line_regex ~r/(?<ntp_timestamp>[\d]+)[\s]+(?<tai_diff>[^\s]+)/
   defp process_line(line) do
-    map = Regex.named_captures(@line_regex, line)
+    line_regex = ~r/(?<ntp_timestamp>[\d]+)[\s]+(?<tai_diff>[^\s]+)/
+    map = Regex.named_captures(line_regex, line)
     h_process_line(map, line)
   end
 
